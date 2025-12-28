93d585893fbfe2b290b03c84e9ff18d127e7f6fb
monwe-jr <monwefrancis@live.com>
2025-12-27 18:36:38 -0700

    Remove unused parameters

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,10 +1,8 @@
-import GitHubIcon from '@material-ui/icons/GitHub'
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
 import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const { name, role, description, resume, social, picture } = about
+  const { name, role, description, picture } = about
 
   return (
     <div className='about center'>
@@ -31,40 +29,6 @@ const About = () => {
       {role && <h2 className='about__role'>A {role}.</h2>}
       <p className='about__desc'>{description && description}</p>
       </div>
-      </div>
-
-      <div className='about__contact center'>
-        {resume && (
-          <a href={resume}>
-            <span type='button' className='btn btn--outline'>
-              Resume
-            </span>
-          </a>
-        )}
-
-        {social && (
-          <>
-            {social.github && (
-              <a
-                href={social.github}
-                aria-label='github'
-                className='link link--icon'
-              >
-                <GitHubIcon />
-              </a>
-            )}
-
-            {social.linkedin && (
-              <a
-                href={social.linkedin}
-                aria-label='linkedin'
-                className='link link--icon'
-              >
-                <LinkedInIcon />
-              </a>
-            )}
-          </>
-        )}
       </div>
     </div>
   )
